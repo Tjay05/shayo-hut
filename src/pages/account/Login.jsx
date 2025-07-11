efac5d5f620961ed87b31c988b43c43c922aaf7f
Tjay05 <tjayolatayo@gmail.com>
2025-07-11 12:57:59 +0100

    Add Login styles

--- src/pages/account/Login.jsx
+++ src/pages/account/Login.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
-    <div className="">
-      <h1 className="text-black">Login</h1>
+    <div className="rule my-12 flex-col">
+      <h1 className="text-black text-3xl font-semibold font-[Poppins] mb-6">Log In</h1>
+
+      <form className="my-8">
+        <label className="label" htmlFor="email">Email</label>
+        <input 
+          type="email" 
+          id="email" 
+          placeholder="Email"
+          className="input-field"
+        />
+        <label className="label" htmlFor="password">Password</label>
+        <input 
+          type="password" 
+          id="password" 
+          placeholder="Password" 
+          className="input-field"
+        />
+        <button className="submit-btn">Log In</button>
+        <p className="text-center text-sm font-normal font-[Inter]">Don't have an account? <Link className="text-[#EF4444] underline" to='signup'>Sign Up</Link></p>
+      </form>
     </div>
   );
 }

70150e93ec391bc87410f61216a50b07c58d8dfa
Tjay05 <tjayolatayo@gmail.com>
2025-07-11 12:58:11 +0100

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+
+import Signup from "./components/Signup"
 import Login from "./pages/account/Login"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Login/>} />
+      <Route path='signup' element={<Signup/>} />
+    </Route>
+  )
+)
 
 function App() {
   return (
     <div className=''>
-      <Login/>
+      <RouterProvider router={router} />
     </div>
   )
 }

e8a871a07a618d88868f571fa7db17de803d13ce
Tjay05 <tjayolatayo@gmail.com>
2025-07-12 15:38:54 +0100

    Add copywright

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-white">
+      <p className="text-center font-dancingscript">&copy; Copywright Shayo Hut 2025</p>
+    </div>
+  );
+}
+ 
+export default Footer;

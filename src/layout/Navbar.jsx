6653b4b3adf1a2ea9bfdb9a949c8cf258fbc4d8d
Tjay05 <tjayolatayo@gmail.com>
2025-07-12 15:36:15 +0100

    Add font variants

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="flex justify-between rule py-4 items-center">
       <div className="flex gap-3 items-center">
         <RxHamburgerMenu color="white" size={25}/>
-        <p className="font-[Dancing_Script] font-extrabold text-white text-2xl">Shayo Hut</p>
+        <p className="font-dancingscript font-extrabold text-white text-2xl">Shayo Hut</p>
       </div>
       <PiNotificationBold color="white" size={25} />
     </header>
